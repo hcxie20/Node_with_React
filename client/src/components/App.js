70f4b177f3c1bc4e89dd69dc4e1aa70a201745a8
hcxie20 <hcxie20@126.com>
2020-02-23 21:34:08 -0800

    add reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,36 @@
-import React from "react"
+import React, {Component} from "react"
+import {BrowserRouter, Route} from 'react-router-dom'
+import {connect} from 'react-redux'
+import * as actions from '../actions'
 
-const App = () => {
-    return (
-        <div>
-            Hi
-        </div>
-    )
+import Header from "./Header"
+
+const Dashboard = () => <h2>DashBoard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser()
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <div className="container">
+                            <Route exact={true} path="/" component={Landing} />
+                            <Route exact={true} path="/surveys" component={Dashboard} />
+                            <Route path="/surveys/new" component={SurveyNew} />
+                        </div>
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    } 
 }
 
-export default App
+export default connect(null, actions)(App)
