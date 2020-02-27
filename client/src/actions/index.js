a95e00c5b351dccf993a57a16489d13844e114ef
hcxie20 <hcxie20@126.com>
2020-02-27 09:50:17 -0800

    path

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,16 +1,18 @@
 import axios from "axios"
 import {FETCH_USER} from "./types"
 
+function serverPath() {
+    const server_path = (process.env.NODE_ENV === "production")? "/app":""
+    return server_path
+}
+
 export const fetchUser = () => async (dispatch) => {
-        console.log(process.env.NODE_ENV)
-        const server_path = (process.env.NODE_ENV === "production")? "/app":""
-        console.log(server_path)
-        const res = await axios.get(server_path + "/api/current_user")
+        const res = await axios.get(serverPath() + "/api/current_user")
         dispatch({type: FETCH_USER, payload: res.data})
     }
 
 export const handleToken = (token) => async (dispatch) => {
-    const res = await axios.post("/api/stripe", token)
+    const res = await axios.post(serverPath() + "/api/stripe", token)
 
     dispatch({type: FETCH_USER, payload: res.data})
 }
