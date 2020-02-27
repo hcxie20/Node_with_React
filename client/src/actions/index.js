30940a3a5935e6d9b085d14082abf344606bd58a
hcxie20 <hcxie20@126.com>
2020-02-27 17:48:03 +0000

    server path set coorectly

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,8 +3,8 @@ import {FETCH_USER} from "./types"
 
 export const fetchUser = () => async (dispatch) => {
         console.log(process.env.NODE_ENV)
-        const server_path = (process.env.NODE_ENV === "Production")? "/app":""
-
+        const server_path = (process.env.NODE_ENV === "production")? "/app":""
+        console.log(server_path)
         const res = await axios.get(server_path + "/api/current_user")
         dispatch({type: FETCH_USER, payload: res.data})
     }
