b93bd4b6cb3a4f246fe928b2434e5edf6b6cbffc
Ubuntu <ubuntu@ip-172-31-11-247.us-west-1.compute.internal>
2020-02-27 17:16:44 +0000

    try adding server side path

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import {FETCH_USER} from "./types"
 
+// const uri = if (process.env.NODE_ENV === "Production")
+
 export const fetchUser = () => async (dispatch) => {
         const res = await axios.get("/api/current_user")
         dispatch({type: FETCH_USER, payload: res.data})
