852820c6e906775a23a9fd40d5e7c4c28142c6ad
hcxie20 <hcxie20@126.com>
2020-02-28 22:01:57 -0800

    server test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,7 @@ import Header from "./Header"
 import Landing from "./Landing"
 import Dashboard from "./Dashboard"
 import SurveyNew from "./surveys/SurveyNew"
+import test from "./test"
 
 class App extends Component {
     componentDidMount() {
